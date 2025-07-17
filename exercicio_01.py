bffad6b4d50b8a0038accfea987c2032e03edc94
tarcisio.mauro <tarcisio.mauro@cloudpay.net>
2025-07-17 14:41:24 +0200

    new solution exercicio 01

--- exercicio_01.py
+++ exercicio_01.py
@@ -1,3 +1,8 @@
 # Crie programa que o usuário digita o seu nome e retorna o número de caracteres
 
-print(len(input("Digite o seu nome: ")))
+# print(len(input("Digite o seu nome: ")))
+
+# better code
+nome = input("Digite o seu nome: ")
+quantidade_caracteres = len(nome)
+print(quantidade_caracteres)
---
+++ exercicio_02.py
@@ -0,0 +1,11 @@
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+
+# print(int(input("Digite um valor: ")) + int(input("Digite outro valor: ")))
+
+numero = 3 # int
+numero_decimal = 4.78 # float
+nome_de_usuario = "Tarcisio" # str
+verdadeiro = True # bool
+
+CONSTANTE = 62535363 # best practice: make constants uppercase
+_SENHA_AWS = 934962482 # adding _ in front makes it secret, use this kind of stuff carefully
